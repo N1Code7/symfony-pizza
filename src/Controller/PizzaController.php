dbd5997954760e3c12cbcc99faea8f7ff9f06e27
N1Code7 <n1code7@outlook.fr>
2022-07-07 14:20:34 +0200

    Creation of PizzaController method to add new pizza in db and display all pizzas

--- src/Controller/PizzaController.php
+++ src/Controller/PizzaController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Pizza;
+use App\Repository\PizzaRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route("/pizza")]
 class PizzaController extends AbstractController
 {
-    #[Route('/pizza', name: 'app_pizza')]
-    public function index(): Response
+    #[Route('/list', name: 'app_pizza_list')]
+    public function displayAllPizza(PizzaRepository $repository): Response
     {
-        return $this->render('pizza/index.html.twig', [
-            'controller_name' => 'PizzaController',
+        $pizzas = $repository->findAll();
+
+        return $this->render("/pizza/list.html.twig", [
+            "pizzas" => $pizzas
         ]);
+    }
+
+    #[Route("/new", name: "app_pizza_new")]
+    public function createPizza(Request $request, PizzaRepository $repository): Response
+    {
+        if (!$request->isMethod("POST")) {
+            return $this->render("pizza/new.html.twig");
+        }
+
+        $name = $request->request->get("name");
+        $desc = $request->request->get("desc");
+        $price = (float)$request->request->get("price");
+        $imageUrl = $request->request->get("imageUrl");
+
+        $pizza = new Pizza();
+        $pizza->setName($name);
+        $pizza->setDescription($desc);
+        $pizza->setPrice($price);
+        $pizza->setImageUrl($imageUrl);
+
+        $repository->add($pizza, true);
+
+        return $this->redirectToRoute("app_pizza_list");
+
+        // return new Response("La pizza possédant l'ID n° {$pizza->getId()} a bien été enregistrée.");
     }
 }
